5745d38fc20f4946bb788f0548755e2be829f355
Evgeniy <melber17@mail.ru>
2022-01-02 19:32:46 +0300

    feat: add splash screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import SplashScreen from "react-native-splash-screen";
 
 import { Navigation } from "./navigation";
 import { WithNavigationContainer } from "./providers";
 
-const App: React.FC = () => (
-	<WithNavigationContainer>
-		<Navigation />
-	</WithNavigationContainer>
-);
+const App: React.FC = () => {
+	useEffect(() => {
+		setTimeout(() => {
+			SplashScreen.hide();
+		}, 1000);
+	}, []);
+
+	return (
+		<WithNavigationContainer>
+			<Navigation />
+		</WithNavigationContainer>
+	);
+};
 
 export default App;
