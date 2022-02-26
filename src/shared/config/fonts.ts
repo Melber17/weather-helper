55fca76f33676929f48c655d1da8ffb98724b8f0
Melber17 <melber17@mail.ru>
2022-02-26 12:54:08 +0300

    feat: add library for scaling & text UI component

---
+++ src/shared/config/fonts.ts
@@ -0,0 +1,9 @@
+export enum Fonts {
+	ROBOTO = "Roboto",
+}
+
+export enum FontStyles {
+	REGULAR = "Regular",
+	MEDIUM = "Medium",
+	BOLD = "Bold",
+}
---
+++ src/shared/ui/Text/ui.tsx
@@ -0,0 +1,36 @@
+
+import React, { FC } from "react";
+import { TextProps as AdditionalTextProps } from "react-native";
+import { moderateScale } from "react-native-size-matters";
+import styled from "styled-components/native";
+
+import { Fonts, FontStyles } from "../../config";
+
+interface IProps extends AdditionalTextProps {
+	children: React.ReactNode;
+	size?: number;
+	color?: string;
+	fontStyle?: FontStyles;
+	font?: Fonts;
+	style?: object;
+}
+
+interface TextProps {
+	size?: number;
+	color?: string;
+	fontStyle?: string;
+	font?: string;
+}
+
+export const Text: FC<IProps> = (props) => {
+	const { size, color, fontStyle, font, children, style } = props;
+
+	return <Wrapper { ...{ size, color, fontStyle, font, style } }>{children}</Wrapper>;
+};
+
+const Wrapper = styled.Text<TextProps>`
+  font-family: ${({ font = Fonts.ROBOTO, fontStyle = FontStyles.REGULAR }) =>
+		`${font}-${fontStyle}`};
+  font-size: ${({ size = 14 }) => moderateScale(size)}px;
+	color: ${({ color = "#03061D" }) => color};
+`;
