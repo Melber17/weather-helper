278771256c90946aa7e08a78c8e6c1a284fd0a30
Melber17 <melber17@mail.ru>
2022-03-10 19:25:29 +0300

    feat: add firebase & setup notifications for android

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 import { AppRegistry } from "react-native";
+import PushNotification from "react-native-push-notification";
 
 import { name as appName } from "./app.json";
 import { App } from "./src/app";
+
+PushNotification.configure({
+	requestPermissions: true
+});
 
 AppRegistry.registerComponent(appName, () => App);
