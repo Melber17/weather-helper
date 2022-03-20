6368531948060099ab18a9ab417e163ca3ae495a
Melber17 <melber17@mail.ru>
2022-03-20 18:14:48 +0300

    refactor: remove unnecessary libs

--- index.js
+++ index.js
@@ -5,7 +5,19 @@ import { name as appName } from "./app.json";
 import { App } from "./src/app";
 
 PushNotification.configure({
-	requestPermissions: true
+	requestPermissions: true,
+	onRegister: function (token) {
+		console.log("TOKEN:", token);
+	},
+	onNotification: function (notification) {
+		console.log("NOTIFICATION:", notification);
+
+		// process the notification
+
+		// (required) Called when a remote is received or opened, or local notification is opened
+		notification.finish(PushNotificationIOS.FetchResult.NoData);
+	},
+
 });
 
 AppRegistry.registerComponent(appName, () => App);
